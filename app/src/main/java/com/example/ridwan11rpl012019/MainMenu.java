0a35b384ea6359433b1211f56109bce74347815d
asyari10 <rasyari42@gmail.com>
2020-09-04 17:43:54 +0700

    add json

--- app/src/main/java/com/example/ridwan11rpl012019/MainMenu.java
+++ app/src/main/java/com/example/ridwan11rpl012019/MainMenu.java
@@ -1,14 +1,26 @@
 package com.example.ridwan11rpl012019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainMenu extends AppCompatActivity {
-
+    CardView menu1;
+    CardView menu2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+        menu1 = (CardView)findViewById(R.id.menu1);
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), listdata.class));
+            }
+        });
+        menu2 = (CardView)findViewById(R.id.menu2);
     }
 }
